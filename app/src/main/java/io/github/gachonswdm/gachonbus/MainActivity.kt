d49824e93b2ebefb9b1d0b176f577d26fc4f52d5
Minjae Seon (Darkhost) <ddol0225@gmail.com>
2019-06-17 21:28:28 +0900

    Add Get Real-time User Location

--- app/src/main/java/io/github/gachonswdm/gachonbus/MainActivity.kt
+++ app/src/main/java/io/github/gachonswdm/gachonbus/MainActivity.kt
@@ -6,17 +6,31 @@ package io.github.gachonswdm.gachonbus
     @date 2019.06.15
  */
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import com.google.android.gms.location.*
 import io.github.gachonswdm.gachonbus.fragment.MainFragment
 import io.github.gachonswdm.gachonbus.fragment.SettingFragment
 import io.github.gachonswdm.gachonbus.fragment.TimelineFragment
 import org.jetbrains.anko.toast
+import java.lang.Exception
 
-class MainActivity : AppCompatActivity() {
+open class MainActivity : AppCompatActivity() {
     private var backPressedTime: Long = 0
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var mLocationRequest: LocationRequest
+    private lateinit var mLocationCallback: LocationCallback
+    private var locationUpdateInterval = (2 * 1000).toLong()
+    private var locationFastestInterval: Long = 2000
+    data class UserLocation(var Latitude: Double, var Longitude: Double)
+    private var mUserLocation: UserLocation = UserLocation(0.0, 0.0)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +40,10 @@ class MainActivity : AppCompatActivity() {
 
         // set Default Fragment
         if(savedInstanceState == null) replaceFragment(MainFragment())
+
+        // User Location Update Check
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        startUserLocationUpdate()
     }
 
     // Set Bottom Navigation Bar
@@ -65,5 +83,48 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.frame, fragment)
             .commit()
+    }
+
+    protected fun startUserLocationUpdate() {
+        mLocationRequest = LocationRequest.create()
+            .setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY)
+            .setInterval(locationUpdateInterval)
+            .setFastestInterval(locationFastestInterval)
+        mLocationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                onLocationChange(locationResult?.lastLocation)
+            }
+        }
+
+        val finePermission: Int = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        if (finePermission == PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationClient.requestLocationUpdates(
+                mLocationRequest,
+                mLocationCallback,
+                null)
+        }
+    }
+
+    fun getUserLastLocation() {
+        val finePermission: Int = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+        val coarsePermission: Int = ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+
+        // Check Location Permission
+        if (finePermission == PackageManager.PERMISSION_GRANTED && coarsePermission == PackageManager.PERMISSION_GRANTED) {
+            // Get Location's Latitude & Longitude
+            mFusedLocationClient.lastLocation.addOnSuccessListener { location : Location? ->
+                if (location != null) {
+                    onLocationChange(location)
+                }
+            }
+            mFusedLocationClient.lastLocation.addOnFailureListener { e: Exception ->
+                e.stackTrace
+            }
+        }
+    }
+
+    fun onLocationChange(location: Location?) {
+        mUserLocation = UserLocation(location!!.latitude, location!!.longitude)
+        Log.d("onLocationChange", "$mUserLocation")
     }
 }
