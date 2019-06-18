d231428f73f0b640247779a2a52ed2b1836af09a
Minjae Seon (Darkhost) <ddol0225@gmail.com>
2019-06-18 09:52:57 +0900

    Re-design Timetable (HTML)

--- app/src/main/java/io/github/gachonswdm/gachonbus/fragment/TimelineFragment.kt
+++ app/src/main/java/io/github/gachonswdm/gachonbus/fragment/TimelineFragment.kt
@@ -10,12 +10,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
 import androidx.fragment.app.Fragment
 import io.github.gachonswdm.gachonbus.R
 
 class TimelineFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_timeline, container, false)
+
+        val webView: WebView = view.findViewById(R.id.timeline_webview)
+        webView.loadUrl("file:///android_asset/www/timeline.html")
         return view
     }
 }
