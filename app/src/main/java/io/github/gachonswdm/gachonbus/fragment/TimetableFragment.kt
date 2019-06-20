424fdcecf7771a66943aeba9bcd7a8075a8cc372
Minjae Seon (Darkhost) <ddol0225@gmail.com>
2019-06-20 19:17:19 +0900

    Rename Timeline to Timetable / Remove Unused File

--- app/src/main/java/io/github/gachonswdm/gachonbus/fragment/TimetableFragment.kt
+++ app/src/main/java/io/github/gachonswdm/gachonbus/fragment/TimetableFragment.kt
@@ -16,9 +16,9 @@ import io.github.gachonswdm.gachonbus.R
 
 class TimetableFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.fragment_timeline, container, false)
+        val view = inflater.inflate(R.layout.fragment_timetable, container, false)
 
-        val webView: WebView = view.findViewById(R.id.timeline_webview)
+        val webView: WebView = view.findViewById(R.id.timetable_webview)
         webView.loadUrl("file:///android_asset/www/timeline.html")
         return view
     }
